b4207edcffdb3d876d9ddcb55dc529287ff28991
Sam Hodge <samuel-hodge@hotmail.co.uk>
2021-02-20 19:00:23 +0000

    creating a react component

--- week2/myfirstreact/src/index.js
+++ week2/myfirstreact/src/index.js
@@ -1,6 +1,12 @@
 import React from "react";
 import ReactDOM from 'react-dom';
 
-const first_element = <h2>Welcome to my first react app</h2>
+//const first_element = <h2>Welcome to my first react app</h2>
 
-ReactDOM.render(first_element, document.getElementById('root'));
+class Student extends React.Component {
+    render() {
+        return <h2>Welcome to my first react app</h2>;
+    }
+}
+
+ReactDOM.render(<Student />, document.getElementById('root'));
