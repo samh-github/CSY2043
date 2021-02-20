18b67f8483855f1f6a30fdb0bdc188b9c9e9fc3d
Sam Hodge <samuel-hodge@hotmail.co.uk>
2021-02-20 18:48:06 +0000

    error resolved

--- week2/myfirstreact/src/index.js
+++ week2/myfirstreact/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from 'react-dom';
 
-const firstelement = <h2>Welcome to my first react app</h2>
+const first_element = <h2>Welcome to my first react app</h2>
 
-ReactDOM.render(firstelement, document.getElementById('main_element'));
+ReactDOM.render(first_element, document.getElementById('root'));
