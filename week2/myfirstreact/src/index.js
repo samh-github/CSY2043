f0656c75f05753ce13e908847c292060c6e65e74
Sam Hodge <samuel-hodge@hotmail.co.uk>
2021-02-20 19:04:23 +0000

    using function components over class components

--- week2/myfirstreact/src/index.js
+++ week2/myfirstreact/src/index.js
@@ -3,10 +3,19 @@ import ReactDOM from 'react-dom';
 
 //const first_element = <h2>Welcome to my first react app</h2>
 
+
+/* #### USING A CLASS COMPONENT ###
 class Student extends React.Component {
     render() {
         return <h2>Welcome to my first react app</h2>;
     }
 }
+ReactDOM.render(<Student />, document.getElementById('root'));
+*/
+
+function Student() {
+    return <h2>Welcome to my first react app</h2>;
+}
+
 
 ReactDOM.render(<Student />, document.getElementById('root'));
